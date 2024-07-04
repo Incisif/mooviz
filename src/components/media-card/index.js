0172e4588cecbacd072a7a9d5e3faee06f033e99
Incisif <loeildejack@gmail.com>
2024-07-04 09:01:01 +0200

    - Add dynamic route for movie details in pages/movies/[id]/page.js -Create MediaCard component

---
+++ src/components/media-card/index.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "./MediaCard.module.scss";
+import Link from "next/link";
+
+const MediaCard = ({ mediaId }) => {
+  return (
+    <div className="w-full min-w-48 font-montserrat shadow-xl transition  duration-300 hover:shadow-2xl">
+      <Link href={`/movies/${mediaId}`}>
+        <div className="relative  aspect-[1/1.41] w-full overflow-hidden  bg-zinc-200">
+          <Image
+            src="https://image.tmdb.org/t/p/w500/hYQs5RPHiWctoYqvI8baHiIqdd8.jpg"
+            alt="media title"
+            fill
+          />
+        </div>
+        <div className="flex flex-col">
+          <h3 className="text-wrap pb-2 text-sm leading-tight">Creed III</h3>
+          <p className="mt-auto pb-2 text-xs font-light">Le 01/03/2023</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
